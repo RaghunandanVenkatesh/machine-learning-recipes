bc25783eb0568871222d049838aaa7da5b6f16a1
Karan Sangha <iamkaransangha@gmail.com>
2016-11-04 23:36:28 -0700

    Decision tree
    Implementation of a basic decision tree with very limited data.
    
    Tested prediction as well. They were correct.

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,11 @@
-import sklearn
+from sklearn import tree
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+#bumpy = 0, smooth = 1
+labels = [0, 0, 1, 1]
+#0 = apple, 1 = orange
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print clf.predict([[90,0]])
+print clf.predict([[190,1]])
